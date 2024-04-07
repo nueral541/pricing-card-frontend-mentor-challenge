6565b75a5453d59c37041f03eb03ac2c9ad3ad8f
nueral541 <142063715+nueral541@users.noreply.github.com>
2024-04-07 11:02:09 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+function alertUser() {
+  if (counter < 8) {
+    const container = document.querySelector('.right ul')
+    const object = document.createElement('li')
+    object.innerText = randomText(Math.floor(Math.random() * 35))
+    container.appendChild(object)
+    counter++
+  } else {
+    const container = document.querySelector('.right ul');
+    container.innerHTML = '';
+    counter = 0
+  }
+}
+
+function randomText(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
